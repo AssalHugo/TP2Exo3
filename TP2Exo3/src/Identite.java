c9c2a490e718cb9ca4fd25b7a3973087fe3e3782
assalloca <hugo.assal1@etu.univ-lorraine.fr>
2023-09-20 10:59:20 +0200

    Ajout de la classe Identite

---
+++ TP2Exo3/src/Identite.java
@@ -0,0 +1,29 @@
+public class Identite {
+
+    /**
+     * attribut nom
+     */
+    private String nom;
+
+    /**
+     * attribut prenom
+     */
+    private String prenom;
+
+    /**
+     * attribut NIP
+     */
+    private String NIP;
+
+    /**
+     * Constructeur de la classe Identite
+     * @param nom
+     * @param prenom
+     * @param NIP
+     */
+    public Identite(String nom, String prenom, String NIP) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.NIP = NIP;
+    }
+}
